9f26e208dd32abd88fa7bcfb0384f41543f91205
Morteza Tavanarad <morteza@Mortezas-MacBook-Pro.local>
2015-12-09 18:36:57 +0800

    fixing npm start bug

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/assets'));
+app.use(express.static(__dirname + '/'));
 
 // views is directory for all template files
 //app.set('views', __dirname + '/views');
